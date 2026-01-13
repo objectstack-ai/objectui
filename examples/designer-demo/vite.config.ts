39eac89ffbc8e2d5f5feb249ac669daff4e16bab
Jack Zhuang <9340100@qq.com>
2026-01-14 02:09:26 +0800

    Update vite.config.ts

--- examples/designer-demo/vite.config.ts
+++ examples/designer-demo/vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@object-ui/designer': path.resolve(__dirname, '../../packages/designer/src'),
+      '@object-ui/components': path.resolve(__dirname, '../../packages/components/src'),
+      '@object-ui/core': path.resolve(__dirname, '../../packages/core/src'),
+      '@object-ui/react': path.resolve(__dirname, '../../packages/react/src'),
+      '@': path.resolve(__dirname, '../../packages/components/src'),
+    }
+  }
 })
